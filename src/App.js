5a2b6041aea5140f989d9d4a608a0c9a4a6e6690
Thanh <thanh.pham5@ntq-solution.com.vn>
2022-02-17 16:19:59 +0700

    add project react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MenuLeft from './components/menu/MenuLeft';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <MenuLeft /><Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/menu/MenuLeft.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/root.css';
+
+export default function MenuLeft() {
+    return (
+        <div className='menu'>
+            <div className='menu-child'><b>PRONEXUS WORKS</b>
+                <div className='menu-child-child'>Web Viewer<br /><br /><br />
+                    書類参照<br /><br />Appsの入手
+                </div>
+            </div>
+        </div>
+    )
+}
